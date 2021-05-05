6c6a9931e03b91bf357bb7b7fa5aff92acee8c7c
szandany <thezandan@gmail.com>
2021-05-05 23:04:15 +0000

    changeLog updated

--- release_1.0.0.sql
+++ release_1.0.0.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql 
 
---changeset SteveZ:createTable_salesTableZ context:"DEV" labels:"Jira1000"
+--changeset SteveZ:createTable_salesTableZ context:DEV labels:Jira1000
 CREATE TABLE salesTableZ (
    ID int NOT NULL,
    NAME varchar(20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
@@ -9,15 +9,15 @@ CREATE TABLE salesTableZ (
 )
 --rollback DROP TABLE salesTableZ
  
---changeset SteveZ:createTable_CustomerInfo context:"DEV" labels:"Jira1001"
+--changeset SteveZ:createTable_CustomerInfo context:DEV labels:Jira1001
 CREATE TABLE CustomerInfo (CustomerTypeID nchar(10) NOT NULL, CustomerDesc nvarchar(MAX))
 --rollback DROP TABLE CustomerInfo
  
---changeset Martha:addPrimaryKey_pk_CustomerTypeID context="PROD" labels="Jira1002"
+--changeset Martha:addPrimaryKey_pk_CustomerTypeID context=PROD labels=Jira1002
 ALTER TABLE CustomerInfo ADD CONSTRAINT pk_CustomerTypeID PRIMARY KEY (CustomerTypeID)
 --rollback ALTER TABLE CustomerInfo DROP CONSTRAINT pk_CustomerTypeID
  
---changeset Amy:CustomerInfo_ADD_address context:"QA" labels:"Jira1009,1010"
+--changeset Amy:CustomerInfo_ADD_address context:QA labels:Jira1009,1010
 ALTER TABLE CustomerInfo ADD address varchar(255)
 --rollback ALTER TABLE CustomerInfo DROP COLUMN address
  
