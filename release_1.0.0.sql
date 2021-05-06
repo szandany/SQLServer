8a81ecdda2c51c6747272562c90a3589f56832c8
szandany <thezandan@gmail.com>
2021-05-06 16:18:53 +0000

    added new changeset

--- release_1.0.0.sql
+++ release_1.0.0.sql
@@ -1,3 +1,12 @@
 --liquibase formatted sql 
 
+--changeset SteveZ:createTable_salesTableZ context:DEV labels:Jira1000
+CREATE TABLE salesTableZ (
+   ID int NOT NULL,
+   NAME varchar(20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+   REGION varchar(20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+   MARKET varchar(20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+)
+--rollback DROP TABLE salesTableZ
+
 
